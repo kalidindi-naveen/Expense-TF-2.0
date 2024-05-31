02654339cf0773dbb7fa798b08fa12ea90068906
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-31 11:51:52 +0000

    Added VPC

---
+++ 00.VPC/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.49.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "step-into-iot-workspacecloud"
+    key            = "step-into-iot-workspace-cloud-vpc-001"
+    region         = "us-east-1"
+    dynamodb_table = "cloud-tbl-001"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ 00.VPC/variables.tf
@@ -0,0 +1,38 @@
+variable "project_name" {
+  type    = string
+  default = "expense"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    Project     = "expense",
+    Environment = "dev",
+    Terraform   = "true"
+  }
+}
+
+variable "cidr_block" {
+  default = "100.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  default = ["100.0.1.0/24", "100.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["100.0.11.0/24", "100.0.12.0/24"]
+}
+
+variable "database_subnet_cidrs" {
+  default = ["100.0.21.0/24", "100.0.22.0/24"]
+}
+
+variable "enable_peering" {
+  default = false
+}
