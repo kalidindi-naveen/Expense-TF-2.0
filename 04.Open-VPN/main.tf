09cfca597c747d81d4fac16ebf3d320813af5a6d
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-31 15:04:30 +0000

    OpenVPN

--- 04.Open-VPN/main.tf
+++ 04.Open-VPN/main.tf
@@ -10,10 +10,11 @@ module "vpn-made-easy" {
   key_name = aws_key_pair.vpn.key_name
   name     = "${var.project_name}-${var.environment}-vpn"
 
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
-  subnet_id              = element(split(",", data.aws_ssm_parameter.pub_subnet_ids.value), 0)
-  ami                    = data.aws_ami.ami_info.id
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [data.aws_ssm_parameter.vpn_sg_id.value]
+  subnet_id                   = element(split(",", data.aws_ssm_parameter.pub_subnet_ids.value), 0)
+  ami                         = data.aws_ami.ami_info.id
 
   tags = merge(
     var.common_tags,
