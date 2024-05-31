0628130dc48c82b05ad07730bf96cc5b86bdfc9d
kalidindi-naveen <step.into.iot@gmail.com>
2024-05-31 13:20:50 +0000

    Added OpenVPN

---
+++ 04.Open-VPN/main.tf
@@ -0,0 +1,24 @@
+resource "aws_key_pair" "vpn" {
+  key_name   = "vpn"
+  public_key = file("/root/.ssh/openvpn.pub")
+  # ~ means windows home directory
+}
+
+
+module "vpn-made-easy" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  key_name = aws_key_pair.vpn.key_name
+  name = "${var.project_name}-${var.environment}-vpn"
+
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
+  subnet_id = element(split(",",data.aws_ssm_parameter.pub_subnet_ids.value), 0)
+  ami = data.aws_ami.ami_info.id
+  
+  tags = merge(
+    var.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}-vpn"
+    }
+  )
+}
