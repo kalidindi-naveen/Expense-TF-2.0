c9c5bcf56b229d47075b69fbf07692dd273345a3
kalidindi-naveen <step.into.iot@gmail.com>
2024-06-01 04:22:28 +0000

    BE

---
+++ 06.EC2-BE/main.tf
@@ -0,0 +1,55 @@
+module "be-made-easy" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "${var.project_name}-${var.environment}-be"
+  ami  = data.aws_ami.ami_id.id
+
+  instance_type          = "t2.micro"
+  monitoring             = true
+  vpc_security_group_ids = [data.aws_ssm_parameter.be_sg_id.value]
+  subnet_id              = element(split(",", data.aws_ssm_parameter.pri_subnet_ids.value), 0)
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-be"
+  })
+}
+
+resource "null_resource" "this" {
+  triggers = {
+    instance_id = module.be-made-easy.id
+  }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = module.be-made-easy.private_ip
+  }
+  provisioner "file" {
+    source      = "${var.common_tags.Component}.sh"
+    destination = "/tmp/${var.common_tags.Component}.sh"
+    # inline = [
+    #     "chmod +x /tmp/${var.common_tags.Component}.sh",
+    #     "sudo sh /tmp/${var.common_tags.Component}.sh ${var.common_tags.Component} ${var.environment}"
+    # ]
+  }
+}
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name = "be-${var.environment}"
+      type = "A"
+      ttl  = 1
+      records = [
+        module.be-made-easy.private_ip
+      ]
+    }
+  ]
+}
