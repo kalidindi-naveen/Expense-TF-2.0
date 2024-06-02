467fad4effd77c168d31019d3a153c8ce06aa402
kalidindi-naveen <step.into.iot@gmail.com>
2024-06-02 04:08:34 +0000

    EC2-BE-ASG

--- 07.EC2-BE-ASG/data.tf
+++ 07.EC2-BE-ASG/data.tf
@@ -10,6 +10,9 @@ data "aws_ssm_parameter" "pri_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/pri_subnet_ids"
 }
 
+data "aws_ssm_parameter" "app_alb_listener_arn" {
+  name = "/${var.project_name}/${var.environment}/app_alb_listener_arn"
+}
 data "aws_ami" "ami_id" {
   most_recent = true
   owners      = ["973714476881"]
