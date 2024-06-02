78f98ca350458276d4d1a9262ebf75d8b8d4bf2f
kalidindi-naveen <step.into.iot@gmail.com>
2024-06-02 05:54:43 +0000

    WEB-ALB

--- 08.WEB-ALB/main.tf
+++ 08.WEB-ALB/main.tf
@@ -39,7 +39,7 @@ module "records" {
 
   records = [
     {
-      name            = "*.app-${var.environment}"
+      name            = "web.app-${var.environment}"
       type            = "A"
       allow_overwrite = true
       alias = {
