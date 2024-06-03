15786cf095405768470a114bbbfffee6dede56fb
kalidindi-naveen <151353708+kalidindi-naveen@users.noreply.github.com>
2024-06-03 08:19:03 +0530

    Install AWS CLI

--- 02.Bastion/bastion.sh
+++ 02.Bastion/bastion.sh
@@ -5,6 +5,10 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
 
-# Install Mysql
+# Install AWS CLI
+dnf install python3-pip
+pip3 install awscli --upgrade --user
+# pip3 uninstall awscli
 
+# Install Mysql
 dnf install mysql -y
